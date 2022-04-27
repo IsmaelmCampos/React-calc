4de66ae3214837213b7fc74de69843b6f41ab922
IsmaelmCampos <104439144+IsmaelmCampos@users.noreply.github.com>
2022-04-27 11:57:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from "./logo.svg";
+
 import React, { useState, Component, useEffect } from "react";
 import "./App.css";
 import { CalculatorInput, CalculatorWrapper } from "./App.styled";
-import { keyframes } from "styled-components";
+
 
 function App() {
-  const [values, setValues] = useState("");
+  const [values, setValues] = useState(0);
 
   let pressedButton = "";
 
-  const renderButton = (calculatorKey) => (
+  const renderButton = (calculatorLines, calculatorKey, i) => (
     <button data-value={calculatorKey} onClick={handleOnclick}>
       {calculatorKey}
     </button>
@@ -28,7 +28,7 @@ function App() {
   ];
 
   const ac = () => {
-    setValues("");
+    setValues(0);
   };
 
   const remove = () => {
@@ -39,14 +39,47 @@ function App() {
   };
 
   const handleOnclick = (e) => {
-    const ops = ["+", "-", "*", "/", ".", "="];
+    const ops = ["+", "*", "/", ".", "="];
 
     pressedButton = e.target?.getAttribute("data-value") || e;
     console.log(typeof pressedButton);
-    values === "" && ops.includes(pressedButton)
-      ? console.log("cannot start with op.")
+
+    ops.includes(values[values.length - 1]) && pressedButton === "."
+      ? console.log("nope")
+      : values === 0 && pressedButton === "-"
+      ? setValues("-")
+      : values === "--" &&
+        values[values.length - 1] === "-" &&
+        pressedButton === "-"
+      ? console.log("cant stack")
+      : values.length > 2 &&
+        values[values.length - 1] === "-" &&
+        pressedButton === "-"
+      ? console.log("cant stack")
+      : values[values.length - 1] === "-" &&
+        ops.includes(values[values.length - 2]) &&
+        ops.includes(pressedButton)
+      ? setValues(values.slice(0, -2) + pressedButton)
+      : values === 0 && pressedButton === "."
+      ? setValues("0.")
+      : pressedButton === "." && values.match(/[\d.]+$/)[0].includes(".")
+      ? console.log("you cant add more dots")
+      : values[values.length - 1] === "-" && pressedButton === "+"
+      ? setValues(values.slice(0, -1) + pressedButton)
+      : values[values.length - 1] === "-" && ops.includes(pressedButton)
+      ? console.log("cant stack operators")
+      : values === 0 && pressedButton === "."
+      ? setValues(values.toString() + ".")
+      : values === 0 && pressedButton === "0"
+      ? console.log("cant stack zeros")
+      : values === 0
+      ? ops.includes(pressedButton)
+        ? setValues(0)
+        : setValues(pressedButton)
+      : values === 0 && ops.includes(pressedButton)
+      ? setValues("")
       : ops.includes(values[values.length - 1]) && ops.includes(pressedButton)
-      ? console.log("error, two operators")
+      ? setValues(values.slice(0, -1) + pressedButton)
       : setValues((values) =>
           pressedButton === "=" &&
           ops.includes(values[values.length - 1]) === false
