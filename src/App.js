c83d02eb9c88ac606971b68877c6adc2c0142c2d
IsmaelmCampos <104439144+IsmaelmCampos@users.noreply.github.com>
2022-04-27 13:14:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-
 import React, { useState, Component, useEffect } from "react";
 import "./App.css";
 import { CalculatorInput, CalculatorWrapper } from "./App.styled";
-
 
 function App() {
   const [values, setValues] = useState(0);
@@ -31,7 +29,7 @@ function App() {
     setValues(0);
   };
 
-const remove = () => {
+  const remove = () => {
     const valstr = values.toString();
     const arrayValue = Array.from(valstr);
     arrayValue.pop();
@@ -40,35 +38,22 @@ const remove = () => {
   };
 
   const handleOnclick = (e) => {
-    const ops = ["+", "*", "/", ".", "="];
+    const ops = ["+", "-", "*", "/", ".", "="];
+    const decimal = [".", ","];
+    pressedButton = e.target?.getAttribute("data-value");
+    console.log(e.target.id);
 
-    pressedButton = e.target?.getAttribute("data-value") || e;
     console.log(typeof pressedButton);
 
-    (ops.includes(values[values.length - 1]) && pressedButton === ".") ||
-    pressedButton === "="
+    pressedButton === "."
+      ? values.toString().match(/[\d.]+$/)
+      : console.log("no way you can add more dots");
+
+    ops.includes(values[values.length - 1]) && pressedButton === "."
       ? console.log("nope")
-      : values === 0 && pressedButton === "-"
-      ? setValues("-")
-      : values === "--" &&
-        values[values.length - 1] === "-" &&
-        pressedButton === "-"
-      ? console.log("cant stack")
-      : values.length > 2 &&
-        values[values.length - 1] === "-" &&
-        pressedButton === "-"
-      ? console.log("cant stack")
-      : values[values.length - 1] === "-" &&
-        ops.includes(values[values.length - 2]) &&
-        ops.includes(pressedButton)
-      ? setValues(values.slice(0, -2) + pressedButton)
       : values === 0 && pressedButton === "."
       ? setValues("0.")
-      : pressedButton === "." && values.match(/[\d.]+$/)[0].includes(".")
-      ? console.log("you cant add more dots")
-      : values[values.length - 1] === "-" && pressedButton === "+"
-      ? setValues(values.slice(0, -1) + pressedButton)
-      : values[values.length - 1] === "-" && ops.includes(pressedButton)
+      : ops.includes(values[values.length - 1]) && ops.includes(pressedButton)
       ? console.log("cant stack operators")
       : values === 0 && pressedButton === "."
       ? setValues(values.toString() + ".")
